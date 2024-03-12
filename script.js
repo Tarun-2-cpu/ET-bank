5d72c72614f96a468eeaeab0b65a181caf63545e
Tarun-2-cpu <146977626+Tarun-2-cpu@users.noreply.github.com>
2024-03-12 16:57:20 +0530

    Add files via upload
    updated

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type",{
+    strings:["AI Analytics","Multi-Currency","E-SIM Magic","XP Wonders","Under 18-Freedom Wallet"],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true
+});
